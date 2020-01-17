78b16dd9fd4b481fc70ef3f79fce531afd7666d8
gongqiang <gongqiang@syswin.com>
2020-01-17 14:22:17 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,30 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 添加到仓库
+git add .
+git commit -m $1
+git pull --rebase
+git push
+
+echo $1
+
 # 生成静态文件
 # npm run docs:build
 yarn docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git init
-git add .
+git init
+git add -A
 git commit -m 'deploy'
-git pull --rebase
 
-# 推送到 branch-deploy 分支
-# git push -f git@github.com:gzwgq222/vuepress.git master:branch-deploy
 
-git push
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:gzwgq222/vuepress.git master:deploy
 
 cd -
