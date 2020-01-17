95644270263e8d6f42c00bee796e6c7e5ce74698
gongqiang <gongqiang@syswin.com>
2020-01-17 15:44:55 +0800

    test: shell color

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,32 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-echo "提交的信息：$1"
+echo -e "\033[32m 提交的信息：$1 \033[0m"
 
-# 添加到仓库
-git add .
-git commit -m "$1"
-git pull --rebase
-git push
-
-
-# 生成静态文件
-# npm run docs:build
+echo -e "\033[32m 开始编译... \033[0m"
+# 本地编译
 yarn docs:build
 
-# 进入生成的文件夹
+echo -e "\033[32m 编译后的文件推送至 deploy 分支... \033[0m"
+# 编译后的文件
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# 编译文件添加至缓存区
 git init
 git add -A
 git commit -m "$1"
 
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 编译文件推送至 deploy 分支
 git push -f git@github.com:gzwgq222/vuepress.git master:deploy
 
+echo -e "\033[32m 源码推送至仓库... \033[0m"
+
 cd -
+# 源码推送至仓库，触发 jenkins 构建
+git add .
+git commit -m "$1"
+git pull --rebase
+git push
+
+echo -e "\033[32m 构建、部署完成！ \033[0m"
